2903932811c2adcef3bc13bc534fa1cdd90ff805
caotsinghua <1577499543@qq.com>
2016-04-04 14:59:05 +0800

    complete、

--- fivechess.js
+++ fivechess.js
@@ -2,6 +2,69 @@ var canvas=document.getElementById("chess");
 var context=canvas.getContext("2d");
 var chesscolor=true;
 var chess=[];
+var over=false;
+
+
+//赢法
+var wins=[];
+//赢法统计数组
+var mywin=[];
+var computerwin=[];
+for (var i = 0; i < 15; i++) {
+	wins[i]=[];
+	for (var j = 0; j < 15; j++) {
+		wins[i][j]=[];//三维数组
+	}
+}
+var count=0;
+
+for (var i = 0; i <15; i++) {
+	for (var j = 0; j < 11; j++) {
+		for(var k=0;k<5;k++)
+		{
+			wins[i][j+k][count]=true;
+		}
+		count++;
+	}	
+}//横着赢法
+for (var i = 0; i <11; i++) {
+	for (var j = 0; j < 15; j++) {
+		for(var k=0;k<5;k++)
+		{
+			wins[i+k][j][count]=true;
+		}
+		count++;
+	}
+}//shu着赢法
+
+for (var i = 0; i < 11; i++) {
+	for(var j=14;j>3;j--)
+	{
+		for(var k=0;k<5;k++ )
+		{
+			wins[i+k][j-k][count]=true;
+		}
+		count++;//所有反斜线赢法
+	}
+}
+for (var i = 0; i < 11; i++) {
+	for(var j=0;j<11;j++)
+	{
+		for(var k=0;k<5;k++ )
+		{
+			wins[i+k][j+k][count]=true;
+		}
+		count++;//所有正斜线赢法
+	}
+}
+console.log(count);
+for (var i = 0; i < count; i++) {
+	mywin[i]=0;
+	computerwin[i]=0;
+}//初始
+
+
+
 for (var i = 0; i < 15; i++) {
 	chess[i]=[];
 	for(j=0;j<15;j++)
@@ -10,15 +73,18 @@ for (var i = 0; i < 15; i++) {
 	}
 }
 
-context.strokeStyle="#bfbfbf";
+context.strokeStyle="#000";
 
-var logo=new Image();
-logo.src="Ts.png";
+// var logo=new Image();
+// logo.src="Ts.png";
 
 window.onload=function(){
-context.drawImage(logo,70,70,300,300);
+// context.drawImage(logo,70,70,300,300);
 drawChessBoard();
+
 }
+
+
 
 function drawChessBoard()
 {
@@ -26,6 +92,7 @@ function drawChessBoard()
 
 		context.moveTo(15+i*30,15);
 	    context.lineTo(15+i*30,450-15);
+
 	    context.stroke();//竖线
 
 	    context.moveTo(15,15+i*30);
@@ -50,27 +117,130 @@ var onestep=function(i,j,chesscolor)
 	context.closePath();
 	context.fill();
 }
+//canvas.addEventListener('click',addchess,false);
 
-canvas.onclick=function(e)
-{
+function addchess(e)
+{   
+	if (over==true) {
+		//context.clearRect(0,0,window.innerWidth,window.innerheight);
+		window.location.reload(true);//重新刷新
+		return;
+	}
+	if (!chesscolor) {
+		return;//不是我下棋
+	}
 	var x=e.offsetX;
 	var y=e.offsetY;
 	var i=Math.floor(x/30);
 	var j=Math.floor(y/30);
-	if (chess[i][j]==0) {
+	if (chess[i][j]==0){
 	onestep(i,j,chesscolor);
-	if(chesscolor)
-	{
-		chess[i][j]=1;
-	}
-	else
-	{
-		chess[i][j]=2;
-	}
-	}
-	chesscolor=!chesscolor;
+	chess[i][j]=1;
+	//if (chesscolor) {//当只有黑子的时候进行判断
+    for (var k = 0;k<count;k++) {
+    	if (wins[i][j][k]) {
+    		mywin[k]++;
+    		computerwin[k]=6;
+             
+    		if (mywin[k]==5) 
+    		{
+    			alert("youwin!");
+    	        over=true;
+    	    }
+    	}
+    }
+//}
+ 	if(!over)
+    	{
+    	   chesscolor=!chesscolor;	
+           computerAI();//棋子变色，我不能下子，电脑下子。下完棋子再变色。结束即退出
+    	}
+    }
+ }    
 
+var computerAI=function(){
+     var myScore=[];
+     var computerScore=[];
+     var max=0;
+     var u=0,v=0;
+     for (var i = 0; i < 15; i++) {
+     	myScore[i]=[];
+     	computerScore[i]=[];
+     	for(var j=0;j<15;j++)
+     	{
+     		myScore[i][j]=0;
+     		computerScore[i][j]=0;
+     	}
+     }
+     for(var i=0;i<15;i++){
+     	for(var j=0;j<15;j++)
+     	{
+     		if (chess[i][j]==0) {//尚未下子
+     			for(var k=0;k<count;k++)
+     			{
+     				if(wins[i][j][k]){
+     					if(mywin[k]==1)
+     					{
+     						myScore[i][j]+=200;
+     					}else if(mywin[k]==2){
+     						myScore[i][j]+=400;
+     					}else if(mywin[k]==3){
+     						myScore[i][j]+=2000;
+     					}else if(mywin[k]==4){
+     						myScore[i][j]+=10000;
+     					}
+     				}
+     						if(computerwin[k]==1)
+     					{
+     						computerScore[i][j]+=220;
+     					}else if(computerwin[k]==2){
+     						computerScore[i][j]+=420;
+     					}else if(computerwin[k]==3){
+     						computerScore[i][j]+=2100;
+     					}else if(computerwin[k]==4){
+     						computerScore[i][j]+=20000;
+     					}
+     				}
+     			}
+     			if (myScore[i][j]>max) {
+     				max=myScore[i][j];
+     				u=i;
+     				v=j;
+     			}else if (myScore[i][j]==max) {
+     				if (myScore[i][j]>computerScore[u][v]) {
+     					u=i;
+     					v=j;
+     				}
+     			if (computerScore[i][j]>max) {
+     				max=computerScore[i][j];
+     				u=i;
+     				v=j;
+     			}else if (computerScore[i][j]==max) {
+     				if (myScore[i][j]>myScore[u][v]) {
+     					u=i;
+     					v=j;
+     				}
+     				
+     			}
+     		}
+     	}
+     }
+     onestep(u,v,false);
+     chess[u][v]=2;
+     for (var k = 0;k<count;k++) {
+    	if (wins[u][v][k]) {
+    		computerwin[k]++;
+    		mywin[k]=6;
+             }
+    		if (computerwin[k]==5) 
+    		{
+    			alert("you lose!");
+    	        over=true;
+    	        
+    	    }
+        }
+        if(!over)
+        chesscolor=!chesscolor;
 }
-
 
 
--- wins.js
+++ wins.js
@@ -1,59 +1,75 @@
-
-//赢法
-var wins=[];
-//赢法统计数组
-var mywin=[];
-var computerwin=[];
-for (var i = 0; i < 15; i++) {
-	wins[i]=[];
-	for (var j = 0; j < 15; j++) {
-		wins[i][j]=[];//三维数组
+var choose=document.getElementById("choose");
+var restart=document.getElementById("restart");
+	restart.onclick=function(){
+		window.location.reload(true);
 	}
-}
-var count=0;
 
-for (var i = 0; i <15; i++) {
-	for (var j = 0; j < 11; j++) {
-		for(var k=0;k<5;k++)
-		{
-			wins[i][j+k][count]=true;
-		}
-		count++;
-	}	
-}//横着赢法
-for (var i = 0; i <11; i++) {
-	for (var j = 0; j < 15; j++) {
-		for(var k=0;k<5;k++)
-		{
-			wins[i+k][j][count]=true;
-		}
-		count++;
-	}
-}//shu着赢法
+var openAI=document.getElementById("openAI");
+openAI.onclick=function(){
+	//window.location.reload(true);
+	canvas.addEventListener('click',addchess,false);
+	choose.style.display="none";
+	canvas.style.display="block";
+}
+var closeAI=document.getElementById("closeAI");
+closeAI.onclick=function(){
+	canvas.removeEventListener('click',addchess,false);
+	canvas.addEventListener('click',oneperson,false);
+	choose.style.display="none";
+	canvas.style.display="block";
+    
+	//window.location.reload(true);
+}
 
-for (var i = 0; i < 11; i++) {
-	for(var j=14;j>3;j--)
-	{
-		for(var k=0;k<5;k++ )
-		{
-			wins[i+k][j-k][count]=true;
-		}
-		count++;//所有反斜线赢法
-	}
-}
-for (var i = 0; i < 11; i++) {
-	for(var j=0;j<11;j++)
-	{
-		for(var k=0;k<5;k++ )
-		{
-			wins[i+k][j+k][count]=true;
-		}
-		count++;//所有正斜线赢法
-	}
-}
-console.log(count);
 
-for (var i = 0; i < count; i++) {
-	mywin[i]=0;
-	computerwin[i]=0;
-}
+function oneperson(e)
+{   
+	if (over==true) {
+		//context.clearRect(0,0,window.innerWidth,window.innerheight);
+		window.location.reload(true);//重新刷新
+		return;
+	}
+	// if (!chesscolor) {
+	// 	return;//不是我下棋
+	// }
+	var x=e.offsetX;
+	var y=e.offsetY;
+	var i=Math.floor(x/30);
+	var j=Math.floor(y/30);
+	if (chess[i][j]==0){
+	onestep(i,j,chesscolor);
+	chess[i][j]=1;
+	if (chesscolor) {//当只有黑子的时候进行判断
+    for (var k = 0;k<count;k++) {
+    	if (wins[i][j][k]) {
+    		mywin[k]++;
+    		computerwin[k]=6;
+             
+    		if (mywin[k]==5) 
+    		{
+    			alert("youwin!");
+    	        over=true;
+    	    }
+    	}
+    }
+ }else{
+ 	for (var k = 0;k<count;k++) {
+    	if (wins[i][j][k]) {
+    		computerwin[k]++;
+    		mywin[k]=6;
+             
+    		if (computerwin[k]==5) 
+    		{
+    			alert("you lose!");
+    	        over=true;
+    	    }
+    	}
+    }
+ }
+ 	if(!over)
+    	{
+    	   chesscolor=!chesscolor;	
+          // computerAI();//棋子变色，我不能下子，电脑下子。下完棋子再变色。结束即退出
+    	}
+    }
+ }    
